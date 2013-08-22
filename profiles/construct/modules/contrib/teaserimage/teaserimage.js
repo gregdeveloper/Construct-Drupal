900b2e07b20023bfcad3ae6a68edd8522f376816
gregsmith <gregsmith@192.168.1.74>
2013-08-22 11:43:14 +0100

    added lightbox2 and teaserimage modules

---
+++ profiles/construct/modules/contrib/teaserimage/teaserimage.js
@@ -0,0 +1,13 @@
+(function ($) {
+Drupal.behaviors.teaserimageFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset#edit-teaserimage', context).drupalSetSummary(function (context) {
+      if ($('#edit-teaserimage-1', context).attr('checked')) {
+        return Drupal.t('Enabled for this type');
+      }
+        return Drupal.t('Disabled for this type');
+    });
+  }
+};
+
+})(jQuery);
