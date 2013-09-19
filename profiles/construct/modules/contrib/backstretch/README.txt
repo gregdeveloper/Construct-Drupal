6206bb298bfbf83327abd75091a49ffbcbda0e1a
gregsmith <gregsmith@192.168.1.74>
2013-09-19 11:08:40 +0100

    Added backstretch module Theme changes Added products feature

---
+++ profiles/construct/modules/contrib/backstretch/README.txt
@@ -0,0 +1,28 @@
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Installation
+
+
+INTRODUCTION
+------------
+
+Current Maintainers:
+  Yannick Leyendecker (http://drupal.org/user/531118)
+  Matt Tucker (http://drupal.org/user/153963)
+
+This module provides a field formatter for jQuery Backstretch [1].
+A simple jQuery plugin that allows you to add a dynamically-resized,
+slideshow-capable background image to any page or element.
+
+INSTALLATION
+------------
+
+1. Install the module the drupal way [2]
+
+2. Select "Backstretch" as field formatter or create a new Context and select
+   "Backstretch" as reaction.
+
+[1] http://srobbin.com/jquery-plugins/backstretch
+[2] http://drupal.org/documentation/install/modules-themes/modules-7
--- profiles/construct/themes/construct/templates/page.tpl.php
+++ profiles/construct/themes/construct/templates/page.tpl.php
@@ -1,48 +1,58 @@
-  <header>
-    <div class="section clearfix">
+<header id="header">
+  <div class="section">
 
-      <h1 id="logo">
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-          <?php if ($logo): ?>
-            <img src="<?php print $logo; ?>" alt="<?php if ($site_name): ?><?php print $site_name; ?><?php else: ?>Home<?php endif; ?>" />
-          <?php elseif ($site_name): ?>
-            <?php print $site_name; ?>
-          <?php else: ?>
-            <?php t('Home'); ?>
-          <?php endif; ?>
-        </a>
-      </h1>
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+    <?php endif; ?>
 
-      <?php print render($page['header']); ?>
-        
-    </div>
-  </header> <!-- /.section, /#header -->
+    <?php print render($page['header']); ?>
 
-  <?php if ($main_menu || $secondary_menu): ?>
-    <nav>
-      <div class="section clearfix">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
-      </div>
-    </nav> <!-- /.section, /#navigation -->
-  <?php endif; ?>
-
-  <?php if ($breadcrumb): ?>
-    <div id="breadcrumb">
-      <?php print $breadcrumb; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php print $messages; ?>
-  
-  <div id="content" class="content">
-    <div class="section clearfix">
-      <?php print render($page['content']); ?>
-    </div>
   </div>
+</header> <!-- /.section, /#header -->
 
-  <footer>
-    <div class="section clearfix">
-      <?php print render($page['footer']); ?>
+<?php if ($main_menu || $secondary_menu): ?>
+  <nav id="navigation">
+    <div class="section">
+      <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
+      <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
     </div>
-  </footer> <!-- /.section, /#footer -->
+  </nav> <!-- /.section, /#navigation -->
+<?php endif; ?>
+
+<?php if ($page['slider']): ?>
+<div id="slider" class="slider">
+  <div class="section">
+    <?php print render($page['slider']); ?>
+  </div>
+</div>
+<?php endif; ?>
+
+<?php if ($page['featured']): ?>
+<div id="news" class="news">
+  <div class="section">
+    <?php print render($page['featured']); ?>
+  </div>
+</div>
+<?php endif; ?>
+
+<?php if ($breadcrumb): ?>
+  <div id="breadcrumb">
+    <?php print $breadcrumb; ?>
+  </div>
+<?php endif; ?>
+
+<?php print $messages; ?>
+
+<div id="content" class="content" role="main">
+  <div class="section">
+    <?php print render($page['content']); ?>
+  </div>
+</div>
+
+<footer id="footer">
+  <div class="section">
+    <?php print render($page['footer']); ?>
+  </div>
+</footer> <!-- /.section, /#footer -->
+
+<?php print render($page['bottom']); ?>
